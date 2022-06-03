cea77d86cea5d3b4c0f8381317134e5e8faf1b7e
Khushi2527 <105801925+Khushi2527@users.noreply.github.com>
2022-06-03 17:55:38 +0530

    Create 3. Divisible Sum Pairs.cpp

---
+++ 3. Divisible Sum Pairs.cpp
@@ -0,0 +1,13 @@
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+    int count=0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+           
+            if(i<j && (ar[i]+ar[j])%k == 0){
+               count++;
+            }              
+        }
+    }
+    return count;
+
+}
