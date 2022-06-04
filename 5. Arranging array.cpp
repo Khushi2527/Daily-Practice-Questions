5fc089cf3f720f0cd2a4f8d50fb45fc55409878e
Khushi2527 <105801925+Khushi2527@users.noreply.github.com>
2022-06-04 16:26:25 +0530

    Create 5. Arranging array.cpp

---
+++ 5. Arranging array.cpp
@@ -0,0 +1,22 @@
+// first negative numbers then positive numbers // 
+void Rearrange(int arr[], int n)
+{
+    vector<int>v;
+    for(int i=0; i<n; i++){
+        if(arr[i]<0){
+            v.push_back(arr[i]);
+            
+        }
+    }
+    for(int i=0; i<n; i++){
+        
+        if(arr[i]>=0){
+             v.push_back(arr[i]);
+        }
+        
+    }
+    for(int i=0; i<n; i++){
+        arr[i]= v[i];
+        
+    }
+}
